e8c5f1d1c97e8ca900f18b5b2bd186dd413ca5e3
YFaris <45187161+YFaris@users.noreply.github.com>
2025-03-01 23:28:16 +0000

    Add files via upload

---
+++ temp/FOUND_UnionSeg_UnionCut/train.py
@@ -0,0 +1,137 @@
+from DataLoader import UnionSegDataset
+import torch.utils.data as Data
+from UnionSeg.model import UnionSeg
+import torch
+import torch.nn as nn
+import numpy as np
+import cv2
+from UnionSeg.crf import densecrf
+import torch.nn.functional as F
+
+
+def setup_seed(seed=3407):
+    import random
+    import os
+    import numpy as np
+    random.seed(seed)  # random seed for python
+    os.environ['PYTHONHASHSEED'] = str(seed)  # random seed for hash
+    np.random.seed(seed)  # numpy random seed
+    torch.manual_seed(seed)  # torch random seed for cpu
+    torch.cuda.manual_seed(seed)  # torch random seed for gpu
+    torch.cuda.manual_seed_all(seed)  # random seed for multi-GPU
+    torch.backends.cudnn.deterministic = True  # deterministic selection
+    torch.backends.cudnn.benchmark = False  # if benchmark=True, deterministic will be False
+    torch.backends.cudnn.enabled = False
+
+
+if __name__ == "__main__":
+    # set hyper parameters
+    seed = 0
+    max_iter = 500
+    nb_epochs = 3
+    batch_size = 50
+    lr0 = 5e-2
+    step_lr_size = 50
+    step_lr_gamma = 0.95
+    w_bs_loss = 1.5
+    w_self_loss = 4.0
+    stop_bkg_loss = 100
+    save_model_freq = 100
+
+    # setup random seed
+    setup_seed(seed)
+
+    # initialize model
+    net = UnionSeg()
+    sigmoid = nn.Sigmoid()
+
+    # initialize training devices
+    train_device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    net.decoder.train()
+    net.decoder.to(train_device)
+
+    # initialize dataset
+    dataset = UnionSegDataset("/home/wzl/DataSet/",
+                              "/media/wzl/T7/PhysicalImageAugmentation/PseudoMaskGenration/DUTS/h5/TokenGraphCutN1_tao0.2_DUTS_Train_ViT_S_16_flexible_8_fixed.h5")
+    loader = Data.DataLoader(
+        dataset=dataset,  # torch TensorDataset format
+        batch_size=batch_size,  # mini batch size
+        shuffle=True,
+        drop_last=True,
+        pin_memory=True)
+
+    # initialize loss function
+    loss_func = nn.BCEWithLogitsLoss()
+
+    # initialize optimizer
+    optimizer = torch.optim.AdamW(
+        net.decoder.parameters(),
+        lr=lr0
+    )
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer,
+        step_size=step_lr_size,
+        gamma=step_lr_gamma,
+    )
+    # train
+    n_iter = 0
+    for epoch in range(nb_epochs):
+        for step, (img_ts, img_inits, mask_gts, img_paths) in enumerate(loader):
+            # move the input to traning device
+            torch_images = img_ts.to(train_device)
+            # Forward steps
+            preds, _, shape_f, att = net(torch_images)
+            # Compute mask detection
+            preds_mask = (sigmoid(preds.detach()) > 0.5).float()
+            # use crf to smooth the mask
+            crf_pred_masks = []
+            for i in range(batch_size):
+                # decode the initial image
+                img_init = np.transpose(img_inits[i].cpu().numpy(), (1, 2, 0))
+                img_init = (img_init * 255).astype(np.uint8)
+                img_init = cv2.cvtColor(img_init, cv2.COLOR_RGB2BGR)
+                # apply crf
+                crf_mask = densecrf(img_init, preds_mask[i, 0].cpu().detach().numpy().astype(np.uint8)).astype(np.uint8)
+                crf_mask = cv2.resize(crf_mask, (224, 224), cv2.INTER_NEAREST)
+                # calculate the IOU between the UnionSeg prediction and UnionCut output
+                UnionCut_mask = mask_gts[i].numpy()
+                intersection = crf_mask * UnionCut_mask
+                union = crf_mask + UnionCut_mask - intersection
+                IoU = np.sum(intersection) / np.sum(union)
+                # use the prediction if IoU > 0.5
+                if IoU >= 0.5:
+                    crf_pred_masks.append(cv2.resize(crf_mask, (28, 28), cv2.INTER_NEAREST)[None, :, :])
+                else:
+                    crf_pred_masks.append(cv2.resize(UnionCut_mask, (28, 28), cv2.INTER_NEAREST)[None, :, :])
+            # make the label of UnionSeg prediction
+            preds_mask = torch.from_numpy(np.stack(crf_pred_masks)).type(torch.LongTensor).to(train_device)
+
+            # Compute loss
+            flat_preds = preds.permute(0, 2, 3, 1).reshape(-1, 1)
+            preds_mask = preds_mask.permute(0, 2, 3, 1).reshape(-1, 1)
+            loss = w_bs_loss * loss_func(flat_preds, preds_mask.float())
+
+            # Apply bkg loss
+            if n_iter < stop_bkg_loss:
+                # pseudo_mask vs preds [loss]
+                flat_labels = F.interpolate(mask_gts.unsqueeze(1), (28, 28)).permute(0, 2, 3, 1).reshape(-1, 1).to(train_device)
+                loss += loss_func(flat_preds, flat_labels.float())
+            # Add regularization when bkg loss stopped
+            else:
+                self_loss = loss_func(flat_preds, preds_mask.float())
+                self_loss = w_self_loss * self_loss
+                loss += self_loss
+            # back propagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+
+            # print progress
+            print('epoch:', epoch, '|step:', step, '|train loss:%.4f' % loss)
+
+            # Save model
+            if n_iter % save_model_freq == 0 and n_iter > 0:
+                net.decoder_save_weights("./module/", n_iter)
+
+            n_iter += 1
---
+++ temp/UnionSeg_LOST.py
@@ -0,0 +1,287 @@
+import cv2
+import numpy as np
+import torch
+from CRF.crf import densecrf
+import LOST.utils as utils
+import LOST.vision_transformer as vits
+from torchvision import transforms as pth_transforms
+import os
+import scipy
+from GraphCut.UnionSeg.inference import UnionSegMaster
+
+
+def preprocess(image, patch_size=16):
+    """
+    preprocess the cv2 image
+    """
+    # transfer the image from bgr to rgb
+    RGB_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+    # pytorch preprocessing
+    transform = pth_transforms.Compose([
+        pth_transforms.ToTensor(),
+        pth_transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+    ])
+    torch_image = transform(RGB_image)
+
+    # obtain the size of the feature map
+    feat_row, feat_col = int(np.ceil(RGB_image.shape[0] / patch_size)), int(
+        np.ceil(RGB_image.shape[1] / patch_size))
+    # Padding the image with zeros to fit multiple of patch-size
+    paded = torch.zeros((3, feat_row * patch_size, feat_col * patch_size))
+    paded[:, :torch_image.shape[1], :torch_image.shape[2]] = torch_image
+    torch_image = paded
+    return torch_image.unsqueeze(0), feat_row, feat_col  # 1, 3, 224, 224
+
+
+def load_pretrained_DINO(path, patch_size=16):
+    # load the model
+    net = vits.vit_small(patch_size=patch_size, num_classes=0)
+    # load the model's pre-trained weight
+    net.cuda()
+    utils.load_pretrained_weights(net, path, checkpoint_key=None, model_name="vit_small",
+                                  patch_size=patch_size)
+    net.eval()
+    return net
+
+
+def DINO_features(net, image, patch_size=16):
+    """
+    return features of all patches at the last ViT block
+    net: the model
+    image: the BGR cv2 image, unnormalized
+    """
+    # preprocess the image
+    torch_image, feat_row, feat_col = preprocess(image, patch_size)
+    torch_image = torch_image.cuda()
+    # obtain the output embeddings of DINO
+    with torch.no_grad():
+        features, qs, ks, vs = net.get_last_qkv(torch_image)
+    return features, qs, ks, vs, feat_row, feat_col
+
+
+def LOST_UnionSeg(net, unionseg, image, which_features="k", k=100, crf=True):
+    """
+    return segmentation mask of the most salient object in the image
+    net: the model
+    image: the BGR cv2 image, unnormalized
+    which_features: the feature to calculate similarity
+    K the number of potential seed to be expanded
+    """
+    # obtain features output by DINO
+    _, qs, ks, vs, feat_row, feat_col = DINO_features(net, image)
+
+    # select the feature to calculate the similarity matrix
+    if which_features == "k":
+        # combine features from multi-head, and remove CLS token
+        fs = ks.permute(0, 2, 1, 3).reshape(1, feat_row * feat_col + 1, -1).squeeze(0)[1:, :]  # 784, 384
+    elif which_features == "q":
+        # combine features from multi-head, and remove CLS token
+        fs = qs.permute(0, 2, 1, 3).reshape(1, feat_row * feat_col + 1, -1).squeeze(0)[1:, :]  # 784, 384
+    elif which_features == "v":
+        # combine features from multi-head, and remove CLS token
+        fs = vs.permute(0, 2, 1, 3).reshape(1, feat_row * feat_col + 1, -1).squeeze(0)[1:, :]  # 784, 384
+    fore_union = unionseg.returnUnion()
+    pad = np.zeros((feat_row * 16, feat_col * 16), dtype=np.uint8)
+    pad[: image.shape[0], : image.shape[1]] = cv2.resize(fore_union, (image.shape[1], image.shape[0]), cv2.INTER_NEAREST)
+    fore_union = cv2.resize(pad, (feat_col, feat_row), cv2.INTER_NEAREST)
+    fore_union = torch.from_numpy(fore_union).cuda().reshape(-1)
+    pred, _, _, _, raw_mask = lost_unionseg(fs, fore_union, (feat_row, feat_col), (16, 16), (3, image.shape[0], image.shape[1]), k_patches=k)
+    mask = cv2.resize(raw_mask, (feat_col * 16, feat_row * 16), cv2.INTER_NEAREST)[: image.shape[0], : image.shape[1]]
+    if crf:
+        # apply crf
+        crf_mask = densecrf(image, mask.astype(np.uint8)).astype(np.uint8)
+        if np.max(crf_mask) != 0:
+            mask = crf_mask
+    return pred, mask
+
+
+def lost_unionseg(feats, fore_union, dims, scales, init_image_size, k_patches=100):
+    """
+    Implementation of LOST method.
+    Inputs
+        feats: the pixel/patche features of an image
+        dims: dimension of the map from which the features are used
+        scales: from image to map scale
+        init_image_size: size of the image
+        k_patches: number of k patches retrieved that are compared to the seed at seed expansion
+    Outputs
+        pred: box predictions
+        A: binary affinity matrix
+        scores: lowest degree scores for all patches
+        seed: selected patch corresponding to an object
+    """
+    # Compute the similarity
+    A = (feats @ feats.T)
+
+    # Compute the inverse degree centrality measure per patch
+    sorted_patches, scores = patch_scoring(A)
+    sorted_patches = sorted_patches[fore_union[sorted_patches] == 1]
+    # Select the initial seed
+    seed = sorted_patches[0]
+
+    # Seed expansion
+    # potentials = sorted_patches[:k_patches]
+    potentials = sorted_patches[:]
+    similars = potentials[A[seed, potentials] > 0.0]
+    M = torch.sum(A[similars, :], dim=0)
+
+    # Box extraction
+    pred, _, raw_mask = detect_box(
+        M, seed, dims, scales=scales, initial_im_size=init_image_size[1:]
+    )
+
+    return np.asarray(pred), A, scores, seed, raw_mask
+
+
+def patch_scoring(M, threshold=0.):
+    """
+    Patch scoring based on the inverse degree.
+    """
+    # Cloning important
+    A = M.clone()
+
+    # Zero diagonal
+    A.fill_diagonal_(0)
+
+    # Make sure symmetric and non nul
+    A[A < 0] = 0
+    C = A + A.t()
+
+    # Sort pixels by inverse degree
+    cent = -torch.sum(A > threshold, dim=1).type(torch.float32)
+    sel = torch.argsort(cent, descending=True)
+
+    return sel, cent
+
+
+def detect_box(A, seed, dims, initial_im_size=None, scales=None):
+    """
+    Extract a box corresponding to the seed patch. Among connected components extract from the affinity matrix, select the one corresponding to the seed patch.
+    """
+    w_featmap, h_featmap = dims
+
+    correl = A.reshape(w_featmap, h_featmap).float()
+
+    # Compute connected components
+    labeled_array, num_features = scipy.ndimage.label(correl.cpu().numpy() > 0.0)
+
+    # Find connected component corresponding to the initial seed
+    cc = labeled_array[np.unravel_index(seed.cpu().numpy(), (w_featmap, h_featmap))]
+
+    # Should not happen with LOST
+    if cc == 0:
+        raise ValueError("The seed is in the background component.")
+
+    # Find box
+    mask = np.where(labeled_array == cc)
+    # initialize mask
+    raw_mask = np.zeros(dims, dtype=np.uint8)
+    raw_mask[mask[0], mask[1]] = 1
+    # Add +1 because excluded max
+    ymin, ymax = min(mask[0]), max(mask[0]) + 1
+    xmin, xmax = min(mask[1]), max(mask[1]) + 1
+
+    # Rescale to image size
+    r_xmin, r_xmax = scales[1] * xmin, scales[1] * xmax
+    r_ymin, r_ymax = scales[0] * ymin, scales[0] * ymax
+
+    pred = [r_xmin, r_ymin, r_xmax, r_ymax]
+
+    # Check not out of image size (used when padding)
+    if initial_im_size:
+        pred[2] = min(pred[2], initial_im_size[1])
+        pred[3] = min(pred[3], initial_im_size[0])
+
+    # Coordinate predictions for the feature space
+    # Axis different then in image space
+    pred_feats = [ymin, xmin, ymax, xmax]
+
+    return pred, pred_feats, raw_mask
+
+
+def visualization(masks):
+    """
+    visualize each component with different color
+    :return:
+    """
+    import random
+    # obtain the number of colours
+    colour_num = masks.shape[0]
+    # generate random colours along each channel
+    R = random.sample(list(range(256)), colour_num)
+    G = random.sample(list(range(256)), colour_num)
+    B = random.sample(list(range(256)), colour_num)
+    # initialize an empty tensor to represent the visualization map
+    visual_map = np.zeros((masks.shape[1], masks.shape[2], 3))
+    # colour each mask
+    for i in range(colour_num):
+        visual_map[masks[i, :, :] == 1] = [R[i], G[i], B[i]]
+    # transfer the map into uint8
+    visual_map = visual_map.astype(np.uint8)
+    return visual_map
+
+
+if __name__ == "__main__":
+    # load the model
+    net = load_pretrained_DINO(path="./dino_deitsmall16_pretrain.pth")
+    image_paths = [
+        "/media/wzl/T7/DataSet/VOC/VOC2007/VOCdevkit/VOC2007/JPEGImages/" + filename for filename in
+        os.listdir("/media/wzl/T7/DataSet/VOC/VOC2007/VOCdevkit/VOC2007/JPEGImages/")]
+    # initialize model
+    UnionSegInstance = UnionSegMaster.load_pretrained_UnionSeg()
+    DinoInstance = UnionSegMaster.load_pretrained_DINO(patch_size=8, scale="base")
+    # image_paths = ["/home/wzl/DataSet/imagenet/train/n07695742/n07695742_6957.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03832673/n03832673_5605.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03134739/n03134739_12697.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n04536866/n04536866_1100.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n04153751/n04153751_15074.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03445777/n03445777_6809.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02504013/n02504013_6379.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02006656/n02006656_8731.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02219486/n02219486_30814.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03888257/n03888257_11187.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n01943899/n01943899_8561.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03888257/n03888257_11580.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03662601/n03662601_10228.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n01537544/n01537544_8631.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02092002/n02092002_15453.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n03792782/n03792782_11989.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02105251/n02105251_1349.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02111500/n02111500_6828.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n04228054/n04228054_1074.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n02108089/n02108089_2929.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n04350905/n04350905_24121.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n01806567/n01806567_18296.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n01981276/n01981276_1560.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n01796340/n01796340_16633.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n04435653/n04435653_5390.JPEG",
+    #                "/home/wzl/DataSet/imagenet/train/n07930864/n07930864_8612.JPEG"]
+    for image_path in image_paths[:]:
+        # load a test image
+        image = cv2.imread(image_path)
+        FeatureMaster = UnionSegMaster(image, tau=0.15, eps=1e-5, UnionSegPatchSize=8, NCutPatchSize=8,
+                                       UnionSegResize=224,
+                                       NCutResize=480, device="cuda:0", pretrained_dino=DinoInstance,
+                                       pretrained_UnionSeg=UnionSegInstance, use_cupy=False)
+        # image = cv2.imread("./CMS_livingroom.PNG")
+        # obtain mask
+        pred, mask = LOST_UnionSeg(net, FeatureMaster, image)
+        cv2.rectangle(
+            image,
+            (int(pred[0]), int(pred[1])),
+            (int(pred[2]), int(pred[3])),
+            (255, 0, 0), 3,
+        )
+        cv2.imshow("image", image)
+        cv2.imshow("mask", 255 * mask)
+        # waiting for user interaction
+        pressedKey = cv2.waitKey(0) & 0xFF
+        if pressedKey == ord('q'):
+            # press q to quit
+            break
+        elif pressedKey == ord('s'):
+            # save the figue and mask
+            cv2.imwrite("./demo/" + image_path.split("/")[-1], image)
+            cv2.imwrite("./demo/" + image_path.split("/")[-1].split(".")[0] + "_mask.jpg", vis)
+    cv2.destroyAllWindows()
